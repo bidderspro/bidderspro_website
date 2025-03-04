cfdf1fcf55a7a134bb5ce7986b18c54a08eba45e
muhammadumarmalik <malikgraphics811@gmail.com>
2025-03-04 23:14:58 +0500

    Update ecosystem configuration for Bidders Pro: change script path, set instances to max, and add development environment

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,25 +4,19 @@ module.exports = {
   apps: [
     {
       name: "bidderspro_website",
-      script: "node_modules/.bin/next",
-      args: "start",
+      script: "node_modules/next/dist/bin/next",
+      args: "start -p 3000",
       cwd: "/home/getmeachai",
-      instances: 1,
+      instances: "max",
       autorestart: true,
+      exec_mode: "cluster",
       watch: false,
       max_memory_restart: "1G",
       env: {
         NODE_ENV: "production",
-        PORT: 3000,
-        GITHUB_ID: "99aae2c73fc53d196807",
-        GITHUB_SECRET: "55c88cefb1ec80c6b1697811f6ff1e287d554249",
-        NEXT_PUBLIC_URL: "http://getmeachai.com",
-        NEXTAUTH_URL: "http://getmeachai.com",
-        NEXTAUTH_SECRET: "sdf",
-        MONGO_URI: "mongodb://localhost:27017/chai",
       },
-      env_production: {
-        NODE_ENV: "production",
+      env_development: {
+        NODE_ENV: "development",
       },
     },
   ],
