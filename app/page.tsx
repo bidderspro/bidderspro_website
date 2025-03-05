c1395a860f1b5d50a8dfa9878170419f9644cc1a
muhammadumarmalik <malikgraphics811@gmail.com>
2025-03-06 01:19:29 +0500

    Refactor logo import in Coming Soon page and update Next.js configuration for unoptimized images

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { motion } from "framer-motion";
 import Head from "next/head";
 import Image from "next/image";
 import { useRef, useEffect } from "react";
-import logo from "../public/assets/images/BP_logo.png";
+import BP_logo from "../public/assets/images/BP_logo.png";
 
 import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram } from "react-icons/fa";
 
@@ -115,7 +115,7 @@ export default function ComingSoon() {
         {/* Logo Section */}
         <div className="flex justify-center md:justify-start">
           <Image
-            src={logo}
+            src={BP_logo}
             width={250}
             height={250}
             alt="BiddersPro Logo"
